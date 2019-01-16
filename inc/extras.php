198fa4865d329ee6ea20fe446f03b8d320172794
lisadebona <annalisa.quiamco@gmail.com>
2019-01-17 00:05:57 +0800

    Updated 404 page

--- inc/extras.php
+++ inc/extras.php
@@ -32,3 +32,135 @@ function acstarter_body_classes( $classes ) {
 	return $classes;
 }
 add_filter( 'body_class', 'acstarter_body_classes' );
+
+function generate_sitemap($pageWithCats=null) {
+    global $wpdb;
+    $lists = array();
+    $menus = wp_get_nav_menu_items('top-menu');
+    $menu_orders = array();
+    $menu_with_children = array();
+    if($menus) {
+        $i=0;
+        foreach($menus as $m) {
+            $page_id = $m->object_id;
+            $menu_title = $m->title;
+            $page_url = $m->url;
+            $post_parent = $m->post_parent;
+            $submenu = array();
+            if($post_parent) {
+                $submenu = array(
+                        'id'=>$page_id,
+                        'title'=>$menu_title,
+                        'url'=>$page_url
+                    );
+                $menu_with_children[$post_parent][$page_id] = $submenu;
+            } else {
+                $menu_orders[$page_id] = $menu_title;
+            } 
+            $i++;
+        }
+    }
+    
+    $results = $wpdb->get_results( "SELECT ID,post_title FROM {$wpdb->prefix}posts WHERE post_type = 'page' AND post_status='publish' AND post_parent=0 ORDER BY post_title ASC", OBJECT );
+    $childPages = $wpdb->get_results( "SELECT ID,post_title,post_parent as parent_id FROM {$wpdb->prefix}posts WHERE post_type = 'page' AND post_status='publish' AND post_parent>0 ORDER BY post_title ASC", OBJECT );
+    $childrenList = array();
+    $childrenAll = array();
+    /* child pages */
+    if($childPages) {
+        foreach($childPages as $cp) {
+            $pId = $cp->parent_id;
+            $iD = $cp->ID;
+            $childrenAll[$iD] = array(
+                                'id'=>$cp->ID,
+                                'title'=>$cp->post_title,
+                                'url'=>get_permalink($iD)
+                            );
+            $childrenList[$pId][] = array(
+                                'id'=>$cp->ID,
+                                'title'=>$cp->post_title,
+                                'url'=>get_permalink($cp->ID)
+                            );
+        }
+    }
+
+    if($results) {
+        foreach($results as $row) {
+            $id = $row->ID;
+            $page_title = $row->post_title;
+            $page_link = get_permalink($id);
+            if(array_key_exists($id,$menu_orders)) {
+                $page_title = $menu_orders[$id];
+            }
+            
+            $lists[$id]['parent_id'] = $id;
+            $lists[$id]['parent_title'] = $page_title;
+            $lists[$id]['parent_url'] = $page_link;
+            
+            if($menu_with_children) {
+
+                $ii_childrens = array();
+                if(array_key_exists($id,$menu_with_children)) {
+                    $ii_childrens = $menu_with_children[$id];
+                    $lists[$id]['children'] = $ii_childrens;
+                }
+
+                /* Show children page even if does not exist on Menu dropdown */
+                if($childrenList && array_key_exists($id, $childrenList)) {
+                    $cc_children = $childrenList[$id];
+                    $exist_children = $lists[$id]['children'];
+                    
+                    foreach($cc_children as $cd) {
+                        $x_id = $cd['id'];
+                        if(!array_key_exists($x_id, $ii_childrens)) {
+                            $addon[$x_id] = $cd;
+                            $exist_children[$x_id] = $cd;
+                        }
+                    } 
+
+                    $lists[$id]['children'] = $exist_children;
+                }
+
+            } else {
+                if($childrenList && array_key_exists($id, $childrenList)) {
+                    $c_obj = $childrenList[$id];
+                    $lists[$id]['children'] = $c_obj;
+                }
+            }
+
+
+            if($pageWithCats) {
+            	foreach($pageWithCats as $p) {
+            		$pageid = $p['id'];
+            		$taxo = $p['taxonomy'];
+            		if($pageid==$id) {
+            			$o_terms = get_terms( array(
+						    'taxonomy' => $taxo,
+						    'hide_empty' => false,
+						) );
+						if($o_terms){
+							foreach ($o_terms as $t) {
+								$term_id = $t->term_id;
+								$term_name = $t->name;
+							}
+							$lists[$id]['categories'] = $o_terms;
+						}
+            		}
+            	}
+            }
+
+            $cat_args = array('hide_empty' => 1, 'parent' => 0, 'exclude'=>array(1));
+            $i_parent_ID = 8; /* Artwork page */
+            $artwork_terms = get_terms( array(
+                'taxonomy' => 'arttypes',
+                'hide_empty' => false,
+            ));
+            if($id == $i_parent_ID) {
+                $lists[$id]['categories'] = $artwork_terms;
+            }
+        }
+
+    }
+    
+    return $lists;
+    
+}
