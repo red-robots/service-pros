4a2353737eb61ac7f2353c01223f9c81218263e2
lisadebona <annalisa.quiamco@gmail.com>
2019-05-29 23:13:18 -0600

    Styling About us page

--- page-about.php
+++ page-about.php
@@ -6,129 +6,150 @@
 
 get_header(); ?>
 
-	<div id="primary" class="full-content-area clear">
+	<div id="primary" class="full-content-area clear aboutContent">
 		<?php while ( have_posts() ) : the_post(); ?>
 			<?php
 				$section_1_image = get_field('section_1_image');
 				$section_1_introduction = get_field('section_1_introduction');
-				$section_1_description = get_field('section_1_description');
-				$section1Style = '';
-				if($section_1_image) {
-					$section1Style = ' style="background-image:url('.$section_1_image['url'].')"';
-				}
 			?>
 
-			<section class="section section_one clear">
-				<div class="column column1"<?php echo $section1Style;?>>
-					<div class="textwrap">
-						<?php echo $section_1_introduction; ?>
-					</div>
+			<section class="pp-intro-text pp-row1 text-center">
+				<div class="wrapper">
+					<?php echo $section_1_introduction ?>
 				</div>
-				<div class="column column2">
-					<div class="textwrap">
-						<?php echo $section_1_description; ?>
+			</section>
+
+			<section class="pp-row2">
+				<div class="wrapper">
+					<div class="flexrow">
+						<?php  
+						for($i=1; $i<=2; $i++) {
+							$ftitle = get_field( "mv_column_".$i."_title" );
+							$ftext = get_field( "mv_column_".$i."_text" ); ?>
+							<?php if ($ftitle && $ftext) { ?>
+							<div class="fbox">
+								<div class="inside">
+									<h3><?php echo $ftitle ?></h3>
+									<?php echo $ftext ?>
+								</div>
+							</div>
+							<?php } ?>
+						<?php } ?>
 					</div>
 				</div>
 			</section>
 
-			<?php
-				$timeline_title = get_field('timeline_title');
-				$timelines = get_field('timeline_');
+			<?php 
+				$va_title = get_field('va_title');
+				$values = get_field('va_info');
 			?>
-			<section class="section section_two clear">
+			<?php if ($va_title || $values) { ?>
+			<section class="pp-row3">
 				<div class="wrapper">
-					<?php if($timeline_title) { ?>
-						<h2 class="section-title"><?php echo $timeline_title;?></h2>
+					<?php if ($va_title) { ?>
+						<h3 class="title"><span><?php echo $va_title ?></span></h3>
 					<?php } ?>
-					<?php if($timelines) { ?>
-					<div class="timelines clear">
-						<div class="vline"><span></span></div>
-						<?php
-							$count_timeline = count($timelines);
-							$i=1; foreach($timelines as $tm) { 
-							$is_align_center = ( isset($tm['align_center']) && $tm['align_center'] ) ? true : false;
-							$divclass = ($i % 2) ? 'odd':'even';
-							if($is_align_center) {
-								$divclass = 'centered';
+					<?php if ($values) { ?>
+					<div class="values-info">
+						<div class="flexrow">
+							<?php foreach ($values as $va) {
+							$icon = $va['icon'];
+							$title = $va['title'];
+							$text = $va['text'];  
+							if($title) {
+								$title = trim(preg_replace('/\s+/',' ', $title));
+								$firstChar = substr($title, 0, 1);
+								$remain = str_replace($firstChar,'',$title);
+								$title = '<strong>'.$firstChar.'</strong>' . $remain;
 							}
-							$i_date = $tm['date'];
-							$i_description = $tm['description'];
-							$i_timeline_image = $tm['timeline_image'];
-							$i_image_position = $tm['image_position'];
 							?>
-
-							<div class="inforow<?php echo $i;?> info-wrapper<?php echo ($is_align_center) ? ' aligned-center':''; ?> <?php echo $i_image_position?>_image<?php echo ($i==1) ? ' first':'';?><?php echo ($i==$count_timeline) ? ' last':'';?>">
-								<div class="info-outer <?php echo $divclass;?>">
-									<div class="info <?php echo $divclass;?>">
-
-									<?php if($i_image_position=='top') { ?>
-
-										<?php if($i_timeline_image) { ?>
-										<div class="timeline-image">
-											<img src="<?php echo $i_timeline_image['url']; ?>" alt="<?php echo $i_timeline_image['title']; ?>" />
-										</div>
+								<?php if ($text) { ?>
+								<div class="vbox">
+									<div class="vinside">
+										<?php if ($icon) { ?>
+										<div class="icon">
+											<span class="image" style="background-image:url('<?php echo $icon['url'] ?>')"></span>
+										</div>	
 										<?php } ?>
-
-										<div class="details clear">
-											<?php if($i_date) { ?>
-												<div class="date"><span><?php echo $i_date ?></span></div>
-											<?php } ?>
-
-											<?php if($i_description) { ?>
-												<div class="description clear"><div class="midtext"><?php echo $i_description ?></div></div>
-											<?php } ?>
-										</div>
-
-									<?php } else { ?>
-
-										<div class="details clear">
-											<?php if($i_date) { ?>
-												<div class="date"><span><?php echo $i_date ?></span></div>
-											<?php } ?>
-
-											<?php if($i_description) { ?>
-												<div class="description clear"><div class="midtext"><?php echo $i_description ?></div></div>
-											<?php } ?>
-										</div>
-
-										<?php if($i_timeline_image) { ?>
-										<div class="timeline-image">
-											<img src="<?php echo $i_timeline_image['url']; ?>" alt="<?php echo $i_timeline_image['title']; ?>" />
-										</div>
+										<?php if ($title) { ?>
+										<div class="vtitle"><?php echo $title ?></div>
 										<?php } ?>
-
-									<?php } ?>
+										<?php if ($text) { ?>
+										<div class="vtext"><?php echo $text ?></div>
+										<?php } ?>
 									</div>
 								</div>
-								<span class="arrow-down"></span>
-							</div>
+								<?php } ?>
+							<?php } ?>
+						</div>
+					</div>
+					<?php } ?>
+				</div>
+			</section>
+			<?php } ?>
 
-						<?php $i++; } ?>
+
+			<?php 
+			$timelineBg = get_field('timeline_image_bg'); 
+			$timeline_title = get_field('timeline_title'); 
+			$timelines = get_field('timeline_'); 
+			$div_bg = '';
+			if($timelineBg){
+				$div_bg = ' style="background-image:url('.$timelineBg['url'].')"';
+			}
+			?>
+			<section class="pp-row4 timeline-section"<?php echo $div_bg ?>>
+				<div class="overlay"></div>
+				<div class="wrapper">
+					<?php if ($timeline_title) { ?>
+						<h2 class="sctitle text-center"><?php echo $timeline_title ?></h2>
+					<?php } ?>
+					<?php if ($timelines) { ?>
+					<div class="timelines-wrap">
+						<div class="noflexrow clear">
+						<?php $j=1; foreach ($timelines as $t) { 
+							$date = $t['date']; 
+							$description = $t['description']; ?>
+							<div data-date="<?php echo $date ?>" class="timeline col<?php echo $j?> <?php echo ($j % 2) ? 'odd':'even'?>">
+								<div class="tmwrap clear">
+								<?php if ($date) { ?>
+									<span class="timeline_date"><?php echo $date ?></span>
+								<?php } ?>
+								<?php if ($description) { ?>
+									<div class="timeline_info"><?php echo $description ?></div>
+								<?php } ?>
+								</div>
+							</div>
+						<?php $j++; } ?>
+						</div>
+						<div class="hline"><div class="line"></div></div>
 					</div>
 					<?php } ?>
 				</div>
 			</section>
 
-
-			<?php
+			<?php 
 				$section_3_image = get_field('section_3_image');
 				$section_3_introduction = get_field('section_3_introduction');
 				$section_3_description = get_field('section_3_description');
-				$section3Style = '';
-				if($section_3_image) {
-					$section3Style = ' style="background-image:url('.$section_3_image['url'].')"';
-				}
 			?>
 
-			<section class="section section_three clear">
-				<div class="column column1"<?php echo $section3Style;?>>
-					<div class="textwrap">
-						<?php echo $section_3_introduction; ?>
+			<section class="pp-row5 partnership-section">
+				<div class="flexrow clear">
+					<div class="imagecol flexbox">
+						<?php if ($section_3_image) { ?>
+							<div class="imagediv" style="background-image:url('<?php echo $section_3_image['url'];?>')"></div>
+						<?php } ?>
+						<?php if ($section_3_introduction) { ?>
+							<div class="intro"><?php echo $section_3_introduction ?></div>
+						<?php } ?>
 					</div>
-				</div>
-				<div class="column column2">
-					<div class="textwrap">
-						<?php echo $section_3_description; ?>
+					<div class="textcol flexbox">
+						<?php if ($section_3_description) { ?>
+							<div class="inside">
+								<div class="text"><?php echo $section_3_description ?></div>
+							</div>
+						<?php } ?>
 					</div>
 				</div>
 			</section>
--- template-parts/banner.php
+++ template-parts/banner.php
@@ -1,8 +1,14 @@
 <?php
-$banner_image = get_field('banner_image');
-?>
+global $post;
+$pages = array('about-us');
+$post_name = $post->post_name;
+$banner_image = get_field('banner_image'); ?>
+
 <?php if($banner_image) { ?>
 <div class="banner clear">
+	<?php if( in_array($post_name, $pages) ) { ?>
+	<h1 class="pagetoptitle"><span><?php echo $post->post_title ?></span></h1>;
+	<?php } ?>
 	<img src="<?php echo $banner_image['url']?>" alt="<?php echo $banner_image['title']?>" />
 </div>
 <?php } ?>
